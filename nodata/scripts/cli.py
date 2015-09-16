c7f65cf064ed154e9fb7353a0f340aa999bb1671
dnomadb <damon@mapbox.com>
2015-09-16 14:49:28 -0700

    updating readme

--- nodata/scripts/cli.py
+++ nodata/scripts/cli.py
@@ -6,16 +6,16 @@ def cli():
     """There's no data like nodata!"""
     pass
 
-@click.command(short_help="Fill seams in imagery / expand valid data area by (inter|extra)polation")
+@click.command(short_help="Blob + expand valid data area by (inter|extra)polation into nodata areas")
 @click.argument('src_path', type=click.Path(exists=True))
 @click.argument('dst_path', type=click.Path(exists=False))
 @click.option('--bidx', '-b', default=None,
-    help="Bands to fill [default = all]")
+    help="Bands to blob [default = all]")
 @click.option('--max-search-distance', '-m', default=4,
-    help="Maximum filling radius [default = 4]")
+    help="Maximum blobbing radius [default = 4]")
 @click.option('--nibblemask', '-n', default=False, is_flag=True,
-    help="Nibble filled nodata areas that are (probably) not seams")
-@click.option('--compress', '-c', default=None, type=click.Choice(['JPEG', 'LZW', 'DEFLATE', None]),
+    help="Nibble blobbed nodata areas [default=False]")
+@click.option('--compress', '-c', default=None, type=click.Choice(['JPEG', 'LZW', 'DEFLATE', 'None']),
     help="Output compression type ('JPEG', 'LZW', 'DEFLATE') [default = input type]")
 @click.option('--mask-threshold', '-d', default=None, type=int,
     help="Alpha pixel threshold upon which to regard data as masked (ie, for lossy you'd want an aggressive threshold of 0) [default=None]")
