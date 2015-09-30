da99ac7328d660c4652747b604256dc0e77b27c0
Sean Gillies <sean@mapbox.com>
2015-09-30 13:29:13 -0400

    Introducing NodataPoolMan
    This class encapsulates execution of masking algorithms over
    windows of a source dataset.

--- nodata/scripts/alpha.py
+++ nodata/scripts/alpha.py
@@ -1,3 +1,5 @@
+from itertools import izip, repeat
+from multiprocessing import cpu_count, Pool
 import sys
 import zlib
 
@@ -5,29 +7,70 @@ import numpy
 import rasterio
 
 
-base_kwds = None
-src = None
+# We're compelled to use global variables for the source dataset and masking
+# algorithm function. The worker initialization and finalization functions
+# make sure that the globals of a worker process are set up and cleaned up
+# properly.
+
+src_dataset = None
+mask_function = None
 
 
-def init_worker(path, profile):
-    global base_kwds, src
-    base_kwds = profile.copy()
-    src = rasterio.open(path)
+def init_worker(path, func):
+    global mask_function, src_dataset
+    mask_function = func
+    src_dataset = rasterio.open(path)
 
 
 def finalize_worker():
-    global base_kwds, src
-    src.close()
-    src = None
-    base_kwds = None
+    global mask_function, src_dataset
+    src_dataset.close()
+    mask_function = None
+    src_dataset = None
 
 
-def compute_window_mask(window, nodata, func, **kwargs):
+# The following function is executed by worker processes.
+
+def compute_window_mask(args):
     """Execute the given function with keyword arguments to compute a valid
     data mask.
 
     Returns the window and mask as deflated bytes.
     """
-    global src
-    source = src.read(window=window, boundless=True)
-    return window, zlib.compress(func(source, nodata, **kwargs))
+    window = args[0]
+    nodata = args[1]
+    extra_args = args[2:]
+    global mask_function, src_dataset
+    source = src_dataset.read(window=window, boundless=True)
+    return window, zlib.compress(mask_function(source, nodata, *extra_args))
+
+
+
+class NodataPoolMan:
+    """Nodata processing pool manager
+
+    This class encapsulates the execution of nodata algorithms on
+    windows of a dataset.
+    """
+
+    def __init__(self, input_path, func, nodata, num_workers=None,
+            max_tasks=100):
+        """Create a pool of workers to process window masks"""
+        self.input_path = input_path
+        self.func = func
+        self.nodata = nodata
+        self.pool = Pool(
+            num_workers or cpu_count()-1, init_worker, (input_path, func),
+            max_tasks)
+
+    def mask(self, windows, *extra_args):
+        """Iterate over windows and compute mask arrays.
+        
+        Yields window, ndarray pairs.
+        """
+        iterargs = izip(windows, repeat(self.nodata), repeat(extra_args))
+        for out_window, data in self.pool.imap_unordered(
+                compute_window_mask, iterargs):
+            yield out_window, numpy.fromstring(
+                                zlib.decompress(data), 'uint8').reshape(
+                                    rasterio.window_shape(out_window))
